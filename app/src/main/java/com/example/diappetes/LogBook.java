500a996b3975cf5c35de97be96a4a39880481b9e
LitC03 <litoxatzi@gmail.com>
2021-12-23 23:29:34 +0200

    Create logbook page and delete action bar

---
+++ app/src/main/java/com/example/diappetes/LogBook.java
@@ -0,0 +1,13 @@
+package com.example.diappetes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogBook extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logbook);
+    }
+}
