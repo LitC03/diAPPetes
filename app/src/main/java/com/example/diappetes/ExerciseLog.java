eb66a30990886a928e303c77b5746aa40e786781
hugofrelin <anders.frelin19@imperial.ac.uk>
2021-12-29 15:57:51 +0100

    Pages for extra notes, exercise log, food log added. New xml template for a large text box made as well.

---
+++ app/src/main/java/com/example/diappetes/ExerciseLog.java
@@ -0,0 +1,13 @@
+package com.example.diappetes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExerciseLog extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise_log);
+    }
+}
---
+++ app/src/main/java/com/example/diappetes/ExtraNotes.java
@@ -0,0 +1,13 @@
+package com.example.diappetes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExtraNotes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.extranotes);
+    }
+}
