7773f2211318ca23737bfef64ee3993978996496
simona <simonabarokova@gmail.com>
2022-01-03 14:43:17 +0200

    new pages (unfinished) - log menu, questionnaire, settings, profile details updated - colors

--- app/src/main/java/com/example/diappetes/ProfileDetails.java
+++ app/src/main/java/com/example/diappetes/ProfileDetails.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LogMenu extends AppCompatActivity {
+public class ProfileDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_menu);
+        setContentView(R.layout.profile_details);
     }
 }
--- app/src/main/java/com/example/diappetes/Questoinnaire.java
+++ app/src/main/java/com/example/diappetes/Questoinnaire.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LogMenu extends AppCompatActivity {
+public class Questoinnaire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_menu);
+        setContentView(R.layout.questoinnaire);
     }
 }
