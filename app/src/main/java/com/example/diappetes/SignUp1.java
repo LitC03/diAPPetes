f97b62f155aa6120194fb6245a16a727a9558ba2
FordImperial <ben.ford19@imperial.ac.uk>
2022-01-05 15:07:04 +0000

    Added data population

--- app/src/main/java/com/example/diappetes/SignUp1.java
+++ app/src/main/java/com/example/diappetes/SignUp1.java
@@ -2,7 +2,6 @@ package com.example.diappetes;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -14,10 +13,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class SignUp1 extends AppCompatActivity {
@@ -26,7 +29,7 @@ public class SignUp1 extends AppCompatActivity {
     private EditText lastName;
     private EditText password;
     private EditText email;
-    private EditText phone;
+    private EditText nhsNum;
     private FirebaseAuth auth;
 
     @Override
@@ -42,7 +45,7 @@ public class SignUp1 extends AppCompatActivity {
         lastName = findViewById(R.id.lastNameField);
         password = findViewById(R.id.passwordField);
         email = findViewById(R.id.emailField);
-        phone = findViewById(R.id.phoneField);
+        nhsNum = findViewById(R.id.nhsNumField);
 
         auth = FirebaseAuth.getInstance();
 
@@ -57,17 +60,24 @@ public class SignUp1 extends AppCompatActivity {
             @Override
             public void onClick(View v){
 
-                String txt_email = email.getText().toString();
+                final String txt_email = email.getText().toString();
                 String txt_password = password.getText().toString();
-                
+                final String txt_nhsNum = nhsNum.getText().toString();
+                final String txt_fName = firstName.getText().toString();
+                final String txt_lName = lastName.getText().toString();
+
+                //At some point this block should be converted to a switch
                 if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
                     Toast.makeText( SignUp1.this, "Please enter a username and password", Toast.LENGTH_SHORT).show();
                 }
-                else if(!validateEmail(txt_email)){
-                    Toast.makeText(SignUp1.this, "Invalid Email Adress", Toast.LENGTH_LONG).show();
+                else if(!validateEmail(txt_email)){ //check the email is valid
+                    Toast.makeText(SignUp1.this, "Invalid Email Address", Toast.LENGTH_LONG).show();
                 }
-                else if(!validatePassword(txt_password)) { //add requirements for special characters here
+                else if(!validatePassword(txt_password)) { //check if a password meets the requirements
                     Toast.makeText(SignUp1.this, "Password must be longer than 8 characters and contain at least 1 special character", Toast.LENGTH_SHORT).show();
+                }
+                else if(txt_nhsNum.length()!=10 || !(isNumeric(txt_nhsNum))){ //check the NHS num is a 10 digit sequence
+                    Toast.makeText(SignUp1.this, "Invalid NHS Number", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     auth.createUserWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener( SignUp1.this,new OnCompleteListener<AuthResult>() {
@@ -75,6 +85,7 @@ public class SignUp1 extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
                                 Toast.makeText(SignUp1.this, "Success", Toast.LENGTH_SHORT).show();
+                                populateUserData(txt_email, txt_fName, txt_lName, txt_nhsNum);
                                 startActivity(new Intent(getApplicationContext(),SignUp2.class));
                             }
                             else{
@@ -89,14 +100,39 @@ public class SignUp1 extends AppCompatActivity {
         });
     }
 
-    private static boolean validateEmail(String email){//check that the email is valid
+    private boolean validateEmail(String email){//check that the email is valid
         String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" //regex magic for a valid email address
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
         return Pattern.compile(regex).matcher(email).matches();
     }
 
-    private static boolean validatePassword(String password){
+    private boolean validatePassword(String password){
         String regex = "[a-zA-Z0-9 ]*"; //more regex magic, this time you check it doesn't match this regex (to prove it contains a character outside the alphanumeric set)
         return (password.length()>7)&&!(Pattern.compile(regex).matcher(password).matches());
+    }
+
+    private void populateUserData(String email, String fName, String lName, String nhsNum){
+        Map<String, Object> patient = new HashMap<>(); //data submitted to Firestore as hashmaps
+
+        patient.put("fName", fName);
+        patient.put("lName", lName);
+        patient.put("email", email);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Patients").document(nhsNum).set(patient).addOnFailureListener(SignUp1.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUp1.this, "Submitting User Data Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private static boolean isNumeric(String str){
+        try {
+            Double.parseDouble(str);
+            return true; //if it can be converted to an int, it is numeric
+        } catch(NumberFormatException e){
+            return false; //if it can't be converted to an int, it is not numeric
+        }
     }
 }
