f6ff86be449226a0feaa50ced4d0ff402ceb1b93
simona <simonabarokova@gmail.com>
2022-01-09 17:28:34 +0000

    updated settings page and created associated pages

--- app/src/main/java/com/example/diappetes/Settings.java
+++ app/src/main/java/com/example/diappetes/Settings.java
@@ -7,10 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Settings extends AppCompatActivity {
 
     Button profileSetButton;
+    Button doctorSetButton;
+    Button deleteAccButton;
+    Button logOutButton;
     Button backButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +24,33 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.settings);
 
         profileSetButton = (Button) findViewById(R.id.ProfileSetBtn);
+        doctorSetButton = (Button) findViewById(R.id.DoctorSetBtn);
+        deleteAccButton = (Button) findViewById(R.id.DeleteAccBtn);
+        logOutButton = (Button) findViewById(R.id.LogOutBtn);
         backButton = (Button) findViewById(R.id.BacktoMainBtn);
 
-        // Button that open Profile Details page
+        // Button that opens Profile Details page
         profileSetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),ProfileDetails.class));
+            }
+        });
+
+        // Button that opens Doctor's Details page
+        doctorSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),DoctorDetails.class));
+            }
+        });
+
+        // Button to log out of account
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Welcome.class));
             }
         });
 
@@ -35,6 +61,17 @@ public class Settings extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
         });
+
+
+        // Button to delete account
+        deleteAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TO BE WRITTEN
+                // maybe add an "are you sure you want to delete your account?" dialogue or something
+            }
+        });
+
 
     }
 }
