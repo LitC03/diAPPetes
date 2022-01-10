065980d0c8ee94559d1462cc998e0c0dc964d9ad
hugofrelin <anders.frelin19@imperial.ac.uk>
2022-01-11 00:04:17 +0100

    Displays personal details

--- app/src/main/java/com/example/diappetes/ProfileDetails.java
+++ app/src/main/java/com/example/diappetes/ProfileDetails.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,7 +23,10 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.regex.Pattern;
+
 public class ProfileDetails extends AppCompatActivity {
+
 
     Button cancelBtn;
     Button saveBtn;
@@ -65,12 +72,12 @@ public class ProfileDetails extends AppCompatActivity {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         Log.d("PD", "DocumentSnapshot data: " + document.getData());
-                        Log.d("PD", "DocumentSnapshot data: " + document.getString("fName"));
                         firstNameEdit.setText(document.getString("fName"));
                         lastNameEdit.setText(document.getString("lName"));
                         emailEdit.setText(document.getString("email"));
                         nhsEdit.setText(document.getString("NHSNumber")); //Cannot change document name
-//                        phoneEdit.setText(document.getString("phoneNume"));
+//                        Get correct names from firebase
+//                        phoneEdit.setText(document.getString("phoneNum"));
 //                        typeDiaEdit.setText(document.getString("Diabetes type"));
 //                        typeInsEdit.setText(document.getString("Insulin type"));
 //                        insAdmEdit.setText(document.getString("Insulin administration"));
@@ -94,10 +101,88 @@ public class ProfileDetails extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String firstNameString = firstNameEdit.getText().toString();
+                final String lastNameString = lastNameEdit.getText().toString();
+                final String emailString = emailEdit.getText().toString();
+                final String nhsString = nhsEdit.getText().toString();
+//                This can be finalised once the signing up is working correctly
+//                final String phoneString = phoneEdit.getText().toString();
+//                final String typeDiaString = typeDiaEdit.getText().toString();
+//                final String typeInsString = typeInsEdit.getText().toString();
+//                final String insAdmString = insAdmEdit.getText().toString();
 
+                if(false) {
+
+                }
+
+                else if(TextUtils.isEmpty(firstNameString)) {
+                    Toast.makeText(ProfileDetails.this, "Please enter your first name", Toast.LENGTH_LONG).show();
+                }
+
+                else if(TextUtils.isEmpty(lastNameString)) {
+                    Toast.makeText(ProfileDetails.this, "Please enter your last name", Toast.LENGTH_LONG).show();
+                }
+
+                else if(TextUtils.isEmpty(emailString)) {
+                    Toast.makeText(ProfileDetails.this, "Please enter your email", Toast.LENGTH_LONG).show();
+                }
+
+                else if(!validateEmail(emailString)) {
+                    Toast.makeText(ProfileDetails.this, "Please enter a correct email", Toast.LENGTH_LONG).show();
+                }
+
+                else if(TextUtils.isEmpty(emailString)) {
+                    Toast.makeText(ProfileDetails.this, "Please enter your email", Toast.LENGTH_LONG).show();
+                }
+
+                else if(nhsString.length()!=10 || !(isNumeric(nhsString))){ //check the NHS num is a 10 digit sequence
+                    Toast.makeText(ProfileDetails.this, "Invalid NHS Number", Toast.LENGTH_LONG).show();
+                }
+
+
+                else{
+                    //All fields can be updated once the signing up is finished
+                    db.collection("Patients").document(global.getNhsNum())
+                            .update(
+                                    "fName", firstNameString,
+                                    "lName", lastNameString,
+                                    "NHSNumber", nhsString,
+                                    "email", emailString)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("PD", "Document updated successfully!");
+                                    Toast.makeText(ProfileDetails.this, "Your details have been update", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(getApplicationContext(), Settings.class));
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(ProfileDetails.this, "Your could not be updated, please try again later", Toast.LENGTH_LONG).show();
+                                    Log.w("PD", "Error updating document", e);
+                                }
+                            });
+
+                }
 
             }
         });
 
+    }
+
+    private boolean validateEmail(String email){//check that the email is valid
+        String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" //regex magic for a valid email address
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        return Pattern.compile(regex).matcher(email).matches();
+    }
+
+    private static boolean isNumeric(String str){
+        try {
+            Double.parseDouble(str);
+            return true; //if it can be converted to an int, it is numeric
+        } catch(NumberFormatException e){
+            return false; //if it can't be converted to an int, it is not numeric
+        }
     }
 }
