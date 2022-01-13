b0b112afb3aa918e5b00abfbc73b4c8f984ddb78
LitC03 <litoxatzi@gmail.com>
2022-01-13 15:56:19 +0200

    SignUp 2 page works now, patient registration to database is done in this activity

--- app/src/main/java/com/example/diappetes/SignUp1.java
+++ app/src/main/java/com/example/diappetes/SignUp1.java
@@ -32,7 +32,6 @@ public class SignUp1 extends AppCompatActivity {
     private EditText password;
     private EditText email;
     private EditText nhsNum;
-    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +47,6 @@ public class SignUp1 extends AppCompatActivity {
         password = findViewById(R.id.PasswordField);
         email = findViewById(R.id.EmailField);
         nhsNum = findViewById(R.id.nhsNumField);
-
-        auth = FirebaseAuth.getInstance();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,95 +65,51 @@ public class SignUp1 extends AppCompatActivity {
                 final String txt_fName = firstName.getText().toString();
                 final String txt_lName = lastName.getText().toString();
 
-                final Global global = (Global) getApplicationContext();
-                global.setNhsNum(txt_nhsNum);
-
-                Task<DocumentSnapshot> checkNHSNumTask = FirebaseFirestore.getInstance().collection("Patients").document(txt_nhsNum).get();
-                //At some point this block should be converted to a switch
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                if(TextUtils.isEmpty(txt_fName) || TextUtils.isEmpty(txt_password)){
                     Toast.makeText( SignUp1.this, "Please enter a username and password", Toast.LENGTH_SHORT).show();
                 }
-                else if(!validateEmail(txt_email)){ //check the email is valid
+                else if(!validateEmail(txt_email)){ //Check the email is valid
                     Toast.makeText(SignUp1.this, "Invalid Email Address", Toast.LENGTH_LONG).show();
                 }
-                else if(!validatePassword(txt_password)) { //check if a password meets the requirements
+                else if(!validatePassword(txt_password)) { //Check if a password meets the requirements
                     Toast.makeText(SignUp1.this, "Password must be longer than 8 characters and contain at least 1 special character", Toast.LENGTH_SHORT).show();
                 }
-                else if(txt_nhsNum.length()!=10 || !(isNumeric(txt_nhsNum))){ //check the NHS num is a 10 digit sequence
+                else if(txt_nhsNum.length()!=10 || !(isNumeric(txt_nhsNum))){ //Check the NHS num is a 10 digit sequence
                     Toast.makeText(SignUp1.this, "Invalid NHS Number", Toast.LENGTH_SHORT).show();
                 }
-                else{
-                    checkNHSNumTask.addOnCompleteListener(SignUp1.this, new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if(checkNHSNumTask.getResult().exists()){ Toast.makeText(SignUp1.this, "NHS Number already used", Toast.LENGTH_SHORT).show();}
-                        else{
-                        auth.createUserWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener( SignUp1.this,new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    Toast.makeText(SignUp1.this, "Success", Toast.LENGTH_SHORT).show();
-                                    global.setUID(auth.getCurrentUser().getUid());
-                                    populateUserData(txt_email, txt_fName, txt_lName, txt_nhsNum, global.getUID());
-                                    startActivity(new Intent(getApplicationContext(),SignUp2.class));
-                                }
-                                else{
-                                    Throwable e = task.getException();
-                                    Toast.makeText(SignUp1.this, "FAIL", Toast.LENGTH_SHORT).show();
-                                    Log.d(e.getClass().getName(), e.getMessage(), e);
-                                }
-                                }
-                            });}
-                            }
-                    });
+                else {
+                    final Global global = (Global) getApplicationContext();
+                    global.setNhsNum(txt_nhsNum);
+
+                    Intent i = new Intent(SignUp1.this, SignUp2.class);
+                    i.putExtra("emailStr", txt_email);
+                    i.putExtra("passStr", txt_password);
+                    i.putExtra("nhsNumStr", txt_nhsNum);
+                    i.putExtra("fNameStr", txt_fName);
+                    i.putExtra("lNameStr", txt_lName);
+                    startActivity(i);
                 }
             }
         });
     }
 
-    private boolean validateEmail(String email){//check that the email is valid
+    private boolean validateEmail(String email){//Check that the email is valid
         String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" //regex magic for a valid email address
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
         return Pattern.compile(regex).matcher(email).matches();
     }
 
     private boolean validatePassword(String password){
-        String regex = "[a-zA-Z0-9 ]*"; //more regex magic, this time you check it doesn't match this regex (to prove it contains a character outside the alphanumeric set)
+        String regex = "[a-zA-Z0-9 ]*"; //More regex magic, this time you check it doesn't match this regex (to prove it contains a character outside the alphanumeric set)
         return (password.length()>7)&&!(Pattern.compile(regex).matcher(password).matches());
-    }
-
-    private void populateUserData(String email, String fName, String lName, String nhsNum, String UID){
-
-        //Create empty array for alerts
-        ArrayList<String> list = new ArrayList<String>();
-
-        Map<String, Object> patient = new HashMap<>(); //data submitted to Firestore as hashmaps
-
-        patient.put("fName", fName);
-        patient.put("lName", lName);
-        patient.put("email", email);
-        patient.put("UID", UID);
-        patient.put("NHSNumber", nhsNum);
-        patient.put("nhsnumber", nhsNum);
-        patient.put("Alerts", list);
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-        db.collection("Patients").document(nhsNum).set(patient).addOnFailureListener(SignUp1.this, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(SignUp1.this, "Submitting User Data Failed", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     private static boolean isNumeric(String str){
         try {
             Double.parseDouble(str);
-            return true; //if it can be converted to an int, it is numeric
+            return true; //If it can be converted to an int, it is numeric
         } catch(NumberFormatException e){
-            return false; //if it can't be converted to an int, it is not numeric
+            return false; //If it can't be converted to an int, it is not numeric
         }
     }
 }
--- app/src/main/java/com/example/diappetes/SignUp2.java
+++ app/src/main/java/com/example/diappetes/SignUp2.java
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -34,12 +35,12 @@ public class SignUp2 extends AppCompatActivity {
     FirebaseFirestore fStore;
     FirebaseAuth fAuth;
     String userID;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup2);
-
 
         diaTypeField = findViewById(R.id.diatype);
         insTypeField = findViewById(R.id.instype);
@@ -51,6 +52,8 @@ public class SignUp2 extends AppCompatActivity {
         backButton = findViewById(R.id.backBtn);
         continueButton = findViewById(R.id.continueBtn);
 
+        auth = FirebaseAuth.getInstance();
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
@@ -61,62 +64,110 @@ public class SignUp2 extends AppCompatActivity {
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String emailStr,passStr,nhsNumStr,fNameStr,lNameStr;
+                emailStr = passStr = nhsNumStr = fNameStr = lNameStr = null;
+
+                //Get strings from previous activity
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    emailStr = extras.getString("emailStr");
+                    passStr = extras.getString("passStr");
+                    nhsNumStr = extras.getString("nhsNumStr");
+                    fNameStr = extras.getString("fNameStr");
+                    lNameStr = extras.getString("lNameStr");
+                }
+
+                //Get all the data from patient
                 final String diabetesType = diaTypeField.getText().toString().trim();
                 final String insulinType = insTypeField.getText().toString().trim();
                 final String insulinAdm = insAdmField.getText().toString();
                 final String docEmail = docEmailField.getText().toString();
                 final String docPhone = docPhoneField.getText().toString();
-                //final String docName = docNameField.getText().toString();
+                final String docName = docNameField.getText().toString();
+                final Global global = (Global) getApplicationContext();
+                final String finalEmailStr = emailStr;
+                final String finalPassStr = passStr;
+                final String finalFNameStr = fNameStr;
+                final String finalLNameStr = lNameStr;
+                final String finalNhsNumStr = nhsNumStr;
 
                 //Display an error if email/password fields are empty
                 if(TextUtils.isEmpty(diabetesType)) {
                     diaTypeField.setError("Diabetes type is a mandatory field!");
                     return;
                 }
-                if(TextUtils.isEmpty(insulinType)) {
+                else if(TextUtils.isEmpty(insulinType)) {
                     insTypeField.setError("Insulin type is a mandatory field!");
                     return;
                 }
 
-                //Add progress bar
-                progressBar.setVisibility(View.VISIBLE);
 
+                //Check if another user already uses the same NHS number
+                Task<DocumentSnapshot> checkNHSNumTask = FirebaseFirestore.getInstance().collection("Patients")
+                        .document(global.getNhsNum()).get();
 
-                    //Add user data to firestore NOT DONE YET
-                try {
-                    userID = fAuth.getCurrentUser().getUid();
-                    DocumentReference documentReference = fStore.collection("Patients").document("Pat_"+userID);
-                    Map<String,Object> data = new HashMap<>();
-                    data.put("Diabetes type",diabetesType);
-                    data.put("Insulin type",insulinType);
-                    data.put("Insulin administration",insulinAdm);
-                    data.put("Alerts", Arrays.asList(docPhone));
-                    data.put("Emergency Contact", Arrays.asList(docEmail));
-
-                    documentReference.set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                checkNHSNumTask.addOnCompleteListener(SignUp2.this, new OnCompleteListener<DocumentSnapshot>() {
                         @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d("TAG", "onSuccess: user Profile is created for Pat_"+ userID);
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            //If there exists an account with the same NHS number, display error message
+                            if(checkNHSNumTask.getResult().exists()){ Toast.makeText(SignUp2.this, "NHS Number already used", Toast.LENGTH_SHORT).show();}
+                            else{
+                                //If no account uses the input NHS number, create new account
+                                auth.createUserWithEmailAndPassword(finalEmailStr, finalPassStr).addOnCompleteListener( SignUp2.this,new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(SignUp2.this, "Success", Toast.LENGTH_SHORT).show();
+                                            global.setUID(auth.getCurrentUser().getUid());
+                                            populateUserData(finalEmailStr, finalFNameStr, finalLNameStr, finalNhsNumStr, global.getUID(),diabetesType,insulinType,insulinAdm,docEmail,docPhone,docName);
+                                            startActivity(new Intent(getApplicationContext(),LogMenu.class));
+                                        }
+                                        else{
+                                            Throwable e = task.getException();
+                                            Toast.makeText(SignUp2.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            Log.d("DB_SignUp2", e.getMessage(), e);
+                                        }
+                                    }
+                                });
+                            }
                         }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d("TAG", "onFailure: "+e.toString());
-                        }
-                    });
-
-                    //Open new page
-                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
-                    //Hide progess bar if user has been added to batabse
-                    progressBar.setVisibility(View.INVISIBLE);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
+                });
             }
         });
     }
 
+    private void populateUserData(String email, String fName, String lName,
+                                  String nhsNum, String UID, String diabetesType, String insulinType,
+                                  String insulinAdm, String docEmail, String docPhone, String docName) {
 
+        //Create empty array for alerts
+        ArrayList<String> list = new ArrayList<String>();
+
+        Map<String, Object> patient = new HashMap<>(); //Data is submitted to Firestore as hashmaps
+
+        patient.put("fName", fName);
+        patient.put("lName", lName);
+        patient.put("email", email);
+        patient.put("UID", UID);
+        patient.put("NHSNumber", nhsNum);
+        patient.put("Alerts", list);
+        patient.put("DType",diabetesType);
+        patient.put("InsType",insulinType);
+        patient.put("InsAdm", insulinAdm);
+        patient.put("DocName", docName);
+        patient.put("DocPhone", docPhone);
+        patient.put("DocEmail", docEmail);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //Add user to Firestore
+        db.collection("Patients").document(nhsNum).set(patient).addOnFailureListener(SignUp2.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUp2.this, "Submitting User Data Failed", Toast.LENGTH_SHORT).show();
+                Log.d("DB_SignUp2",e.getMessage());
+            }
+        });
+    }
 }
