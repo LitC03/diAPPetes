2e8484981b7ef2679a3b663257fa8624c92f9689
LitC03 <litoxatzi@gmail.com>
2022-01-15 09:56:31 +0200

    Connectivity error is added to MainActivity

--- app/src/main/java/com/example/diappetes/Activities/MainActivity.java
+++ app/src/main/java/com/example/diappetes/Activities/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.diappetes.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.diappetes.R;
@@ -19,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Internet Error
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()){
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.internet_dialog);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+            dialog.getWindow().getAttributes().windowAnimations=
+                    android.R.style.Animation_Dialog;
+            Button TryAgain = dialog.findViewById(R.id.errorBtn);
+            TryAgain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    recreate();
+                }
+            });
+            dialog.show();
+        }
 
         // Button that opens the Log Menu
         logBookBtn = (Button) findViewById(R.id.LogBookBtn);
